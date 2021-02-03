bb570bb20df81f9f44d64644168354a1638f76d1
Stephen Becker <stephen.beckr@gmail.com>
2021-02-03 10:01:20 -0700

    update HW directories

--- Homeworks/HW10/my_upsample.m
+++
@@ -1,5 +0,0 @@
-function x = my_upsample( y, sampleSet, n )
-% x = my_upsample( y, sampleSet, n )
-%   returns x of length n such that x(sampleSet) = y
-x   = zeros(n,size(y,2));
-x(sampleSet,:)  = y;
